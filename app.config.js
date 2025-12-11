1414934c3925f1538a1901c505c10ebc8a0e1e4c
unknown <biigideon@gmail.com>
2025-12-11 15:08:38 +0300

    Simplify config - remove asset references

--- app.config.js
+++ app.config.js
@@ -4,14 +4,7 @@ export default {
     slug: "momentum",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
     userInterfaceStyle: "light",
-    splash: {
-      // 注释掉自定义启动图，使用默认配置
-      // image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
     assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
@@ -19,12 +12,7 @@ export default {
     },
     android: {
       package: "com.protectpesa.momentum",
-      versionCode: 1,
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
-      permissions: []
+      versionCode: 1
     },
     extra: {
       eas: {
